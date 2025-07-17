7d206d0b886fd3522f8bc28400fd553797895314
BenjaminOrtiz <benjamin.ortiz0729@gmail.com>
2025-07-17 12:53:50 +0200

    fix some issue

--- src/Contract/contract.js
+++ src/Contract/contract.js
@@ -4,7 +4,10 @@ import contractABI from './abis/BellshireProxy.json'; // copy ABI to src/abis
 const contractAddress = process.env.REACT_APP_PROXY_CONTRACT_ADDRESS;
 
 export const getContract = () => {
+  if (!window.ethereum) {
+    console.error("MetaMask not detected (window.ethereum is undefined)");
+    return null;
+  }
   const web3 = new Web3(window.ethereum);
   return new web3.eth.Contract(contractABI.abi, contractAddress);
-
 };
